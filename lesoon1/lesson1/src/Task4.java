536d3f14a0981dc459a88cf30e617f89f32e7c8d
ASF <leha.flyagin@mail.ru>
2023-07-07 17:06:45 +0300

    home1.1

--- lesoon1/lesson1/src/Task4.java
+++ lesoon1/lesson1/src/Task4.java
@@ -1,2 +1,29 @@
-public class Task4 {
-}
+import java.util.Arrays;
+
+    public class Task4 {
+        public static void main(String[] args) {
+            String inputStr = "2? + ?5 = 69";
+            findSolution(inputStr);
+
+        }
+        public static boolean findSolution(String inputStr){
+            String [] arrStr = inputStr.split(" ");
+            Arrays.toString(arrStr);
+            for(int i = 0; i < 10; i++){
+                for(int j = 0; j<10; j++){
+                    int q = Integer.parseInt(arrStr[0].replace("?", String.valueOf(i)));
+                    int w = Integer.parseInt(arrStr[2].replace("?", String.valueOf(j)));
+                    if(q+w==Integer.parseInt(arrStr[4])){
+                        System.out.println(q + " + " + w + " = " + Integer.parseInt(arrStr[4]));
+                        return true;
+                    }
+                }
+            }
+            System.out.println("нет решений");
+            return false;
+        }
+
+
+
+    }
+
